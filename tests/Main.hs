7b358895c880a28ea146373b0d79e75b208bdc4c
Evan Rutledge Borden <eborden@frontrowed.com>
2022-06-01 10:29:01 -0500

    Remove freckle-app and extra-deps

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,13 +1,1 @@
-module Main
-  ( main
-  )
-where
-
-import Prelude
-
-import Freckle.App.Test.Hspec.Runner (runParConfig)
-import qualified Spec
-import Test.Hspec
-
-main :: IO ()
-main = "freckle-app" `runParConfig` parallel Spec.spec
+{-# OPTIONS_GHC -F -pgmF hspec-discover -optF --module-name=Main #-}
